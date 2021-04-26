9fc4dab742f2f61c1ac8f256e8987ba8539715a6
Muhammad Zohair <zohair739@gmail.com>
2021-04-26 23:19:09 +0500

    images added for logo and deleted useless images

--- app/src/main/java/com/lunatialiens/incidentreportingsystem/views/fragments/HomeFragment.java
+++ app/src/main/java/com/lunatialiens/incidentreportingsystem/views/fragments/HomeFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -14,19 +13,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.lunatialiens.incidentreportingsystem.R;
 import com.lunatialiens.incidentreportingsystem.repository.CurrentDatabase;
 
-import org.joda.time.DateTime;
-
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
-    private TextView nameTextView;
-
-    private ImageView leftArrowImageView;
-    private TextView currentDayTextView;
-    private ImageView rightArrowImageView;
-
     private FloatingActionButton addFloatingActionButton;
-
-    private DateTime currentDate;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,14 +23,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        currentDate = new DateTime();
-
-        nameTextView = v.findViewById(R.id.tv_name);
-
-        leftArrowImageView = v.findViewById(R.id.iv_left_arrow);
-        currentDayTextView = v.findViewById(R.id.tv_current_day);
-        rightArrowImageView = v.findViewById(R.id.iv_right_arrow);
-
+        TextView nameTextView = v.findViewById(R.id.tv_name);
         if (CurrentDatabase.getCurrentPublicUser() != null) {
             nameTextView.setText(CurrentDatabase.getCurrentPublicUser().getName());
         }
@@ -49,10 +31,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         addFloatingActionButton = v.findViewById(R.id.fab_add);
         addFloatingActionButton.setOnClickListener(this);
 
-        leftArrowImageView.setOnClickListener(this);
-        rightArrowImageView.setOnClickListener(this);
-
-        currentDayTextView.setText(currentDate.toString("dd/MM/yyyy"));
         return v;
     }
 
@@ -65,11 +43,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v == addFloatingActionButton) {
-        } else if (v == leftArrowImageView) {
-
-        } else if (v == rightArrowImageView) {
-            currentDate = currentDate.plusDays(1);
-            currentDayTextView.setText(currentDate.toString("dd/MM/yyyy"));
         }
     }
 }
